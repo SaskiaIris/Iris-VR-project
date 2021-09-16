a96ee4848fe79196430012eca1da0be6077736c2
Iris <irisoostra@upcmail.nl>
2021-09-16 21:52:21 +0200

    aan het hopen dat de errors zichzelf oplossen

--- Assets/Scripts/FallingTreats.cs
+++ Assets/Scripts/FallingTreats.cs
@@ -10,28 +10,32 @@ public class FallingTreats : MonoBehaviour
     [SerializeField]
     private float spawnDelay = 2.0f;
 
-    private float startTime;
-
     private int treatNumberToSpawn;
     private GameObject treatToSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        Time.timeScale = 1.0f;
-        startTime = Time.deltaTime;
+        StartCoroutine(SpawnTreats());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.deltaTime + spawnDelay >= startTime) {
-            treatNumberToSpawn = Random.Range(0, treats.Length);
-            treatToSpawn = treats[treatNumberToSpawn];
+        
+    }
 
-            Instantiate(treatToSpawn, transform.position, transform.rotation);
+    IEnumerator SpawnTreats() {
+        treatNumberToSpawn = Random.Range(0, treats.Length);
+        treatToSpawn = treats[treatNumberToSpawn];
 
-            startTime = Time.deltaTime;
-        }
+        Vector3 randomRotation = transform.eulerAngles;
+        randomRotation.y = Random.Range(0f, 360f);
+
+        treatToSpawn.transform.eulerAngles = randomRotation;
+
+        Instantiate(treatToSpawn, transform.position, treatToSpawn.transform.rotation);
+
+        yield return new WaitForSeconds(spawnDelay);
     }
 }

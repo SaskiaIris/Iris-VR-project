6e2657b24b6e1f68a4de3aa5a61f997c0a2b5ed4
Iris <irisoostra@upcmail.nl>
2021-09-16 20:48:29 +0200

    Prefabs renaming

--- Assets/Scripts/FallingTreats.cs
+++ Assets/Scripts/FallingTreats.cs
@@ -5,17 +5,33 @@ using UnityEngine;
 public class FallingTreats : MonoBehaviour
 {
     [SerializeField]
-    GameObject[] treats = new GameObject[9];
+    private GameObject[] treats = new GameObject[8];
+
+    [SerializeField]
+    private float spawnDelay = 2.0f;
+
+    private float startTime;
+
+    private int treatNumberToSpawn;
+    private GameObject treatToSpawn;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        Time.timeScale = 1.0f;
+        startTime = Time.deltaTime;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(Time.deltaTime + spawnDelay >= startTime) {
+            treatNumberToSpawn = Random.Range(0, treats.Length);
+            treatToSpawn = treats[treatNumberToSpawn];
+
+            Instantiate(treatToSpawn, transform.position, transform.rotation);
+
+            startTime = Time.deltaTime;
+        }
     }
 }
